0c7e27b9d5acf4fd6fdc8274d0563208715ed8da
mattRepos <matveyderudjov@gmail.com>
2024-12-10 19:13:53 +0700

    removed test.py, updated .gitignore

--- completed_forms/test.py
+++
@@ -1,9 +0,0 @@
-from forms.repository import db
-from forms.services.forms_service import FormsService
-
-service = FormsService(db.forms)
-
-service.get_form_by_fields({
-    "zalupa": "alskjdlas@mail.com",
-    "penis": "+79831656419"
-})
